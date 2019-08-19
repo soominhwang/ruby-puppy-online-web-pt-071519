163fbc9463491a06e64af6b9ec02396bf6f8e5ac
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-19 05:47:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
 # Add your code here
 
 class Dog
- 
+
   @@all = []
- 
+
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
     @@all << self
   end
- 
+
   def self.all
     @@all
   end
